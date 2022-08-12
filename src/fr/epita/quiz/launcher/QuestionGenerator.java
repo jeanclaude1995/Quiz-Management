25a22e818e5bc81f6415a15abc14ef0a2d773c1d
jeanclaude1995 <77775226+jeanclaude1995@users.noreply.github.com>
2022-08-12 15:54:35 +0200

    Revert "2nd commit"
    This reverts commit 3cd661ab6d76aa97206794c2b65142b0919cfc22.

--- src/fr/epita/quiz/launcher/QuestionGenerator.java
+++ src/fr/epita/quiz/launcher/QuestionGenerator.java
@@ -4,8 +4,6 @@ import fr.epita.quiz.datamodel.Choice;
 import fr.epita.quiz.datamodel.MCQQuestion;
 import fr.epita.quiz.datamodel.OpenQuestion;
 import fr.epita.quiz.datamodel.Question;
-import fr.epita.quiz.services.data.dao.MCQDAO;
-import fr.epita.quiz.services.data.dao.OpenQuestionDAO;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -20,11 +18,9 @@ public class QuestionGenerator {
     public ArrayList<Question> getallQuestions(String quizType,String topic, int qnPerTopic) throws SQLException {
         switch (quizType) {
             case "MCQ":
-                MCQDAO mcqDAO = new MCQDAO();
-                return mcqDAO.getMCQQuestions(topic, qnPerTopic);
+                return getMCQQuestions(topic, qnPerTopic);
            case "Open":
-                OpenQuestionDAO openQuestionDAO = new OpenQuestionDAO();
-                return openQuestionDAO.getOpenQuestions(topic, qnPerTopic);
+                return getOpenQuestions(topic, qnPerTopic);
 //         case "Associative":
 //              return getAssociativeQuestions(topic, qnPerTopic);
             default:
@@ -34,6 +30,66 @@ public class QuestionGenerator {
 
     }
 
+    private ArrayList<Question> getMCQQuestions(String topic, int qnPerTopic) throws SQLException {
+
+        String SQL = "SELECT * FROM quiz where topic ='"+topic+"'order by random() limit "+qnPerTopic;
+
+        ArrayList<Question> questions = new ArrayList<Question>();    // ArrayList to store questions
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            while (rs.next()) {
+                MCQQuestion question = new MCQQuestion();
+                question.setDescription(rs.getString("description"));
+                question.setTitle(rs.getString("Question"));
+                question.setTopic(rs.getString("topic"));
+                Choice answer = new Choice();
+                answer.setTitle(rs.getString("answer"));
+                answer.setCorrect(true);
+                Choice choice2 = new Choice();
+                choice2.setTitle(rs.getString("choice2"));
+                choice2.setCorrect(false);
+                Choice choice3 = new Choice();
+                choice3.setTitle(rs.getString("choice3"));
+                choice3.setCorrect(false);
+                Choice choice4 = new Choice();
+                choice4.setTitle(rs.getString("choice4"));
+                choice4.setCorrect(false);
+                ArrayList<Choice> choices = new ArrayList<>();
+                choices.add(answer);
+                choices.add(choice2);
+                choices.add(choice3);
+                choices.add(choice4);
+                Collections.shuffle(choices);
+                question.setChoices(choices);
+                questions.add(question);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return questions;
+
+    }
+    private ArrayList<Question> getOpenQuestions(String topic, int qnPerTopic) throws SQLException {
+
+        String SQL = "SELECT * FROM open_questions where topic ='"+topic+"'order by random() limit "+qnPerTopic;
+        ArrayList<Question> questions = new ArrayList<Question>();    // ArrayList to store questions
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            while (rs.next()) {
+                OpenQuestion question = new OpenQuestion();
+                //question.setDescription(rs.getString("hint"));
+                question.setTitle(rs.getString("Question"));
+                question.setTopic(rs.getString("topic"));
+                question.setAnswer(rs.getString("answer"));
+                questions.add(question);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return questions;
+    }
 //    private ArrayList<Question> getAssociativeQuestions(String topic, int qnPerTopic) throws SQLException {
 //
 //        String SQL = "SELECT * FROM fill_in_the_blank where topic ='"+topic+"'order by random() limit "+qnPerTopic;

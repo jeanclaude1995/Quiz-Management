4406051e7753c7de161dc813e0152adffbc955fe
jeanclaude1995 <77775226+jeanclaude1995@users.noreply.github.com>
2022-08-12 15:55:18 +0200

    Revert "Revert "2nd commit""
    This reverts commit 25a22e818e5bc81f6415a15abc14ef0a2d773c1d.

--- src/fr/epita/quiz/launcher/Launcher.java
+++ src/fr/epita/quiz/launcher/Launcher.java
@@ -3,9 +3,7 @@ package fr.epita.quiz.launcher;
 import fr.epita.quiz.datamodel.MCQQuestion;
 import fr.epita.quiz.datamodel.Question;
 import fr.epita.quiz.datamodel.Quiz;
-import fr.epita.quiz.services.data.dao.QuizEvaluation;
-import fr.epita.quiz.services.data.dao.StudentLogin;
-import fr.epita.quiz.services.data.dao.StudentQuiz;
+import fr.epita.quiz.services.data.dao.*;
 
 import java.io.*;
 import java.sql.SQLException;
@@ -21,30 +19,99 @@ public class Launcher {
         StudentQuiz studentQuiz = new StudentQuiz();
         Quiz questionGenerator = new Quiz();
         String quizType = studentQuiz.getQuizTypeFromStudent(scan);
+        //String quizType = "Open";
         if (quizType != null) {
-            ArrayList<String> topics = studentQuiz.getAllTopicsFromStudent(scan);
-            int numberOfQuestions = studentQuiz.getTotalQuestionsCountFromStudent(scan);
-            ArrayList<Question> questions = questionGenerator.getQuestions(quizType,topics, numberOfQuestions);
-            QuizEvaluation quizEvaluation = new QuizEvaluation();
-            int score = 0;
-            int position = 0;
-            File file  = new File("D://Code2.txt");
-            FileOutputStream outputStream = new FileOutputStream(file);
-            PrintWriter pw = new PrintWriter(outputStream);
-            pw.println("Quiz Type: " + quizType+ "QUESTIONS");
-            for (Question question : questions) {
-                position++;
-                pw.println(position + ". " + question.getTitle());
-                boolean isCorrect = quizEvaluation.displayAndGetAnswer(pw,position,question);
-                if (isCorrect) score++;
+            //CRUD operations for quiz table
+            System.out.println("1. Create");
+            System.out.println("2. Read");
+            System.out.println("3. Update");
+            System.out.println("4. Delete");
+            System.out.println("5. Exit");
+            System.out.println("Enter your choice");
+            String choice = scan.nextLine();
+            int choiceInt = Integer.parseInt(choice);
+            //int choiceInt = 3;
+            switch (choiceInt) {
+                case 1:
+                    if (quizType.equals("MCQ")) {
+                        MCQDAO mcqDAO = new MCQDAO();
+                        mcqDAO.create(scan);
+                    } else if(quizType.equals("Open")){
+                        OpenQuestionDAO openQuestionDAO = new OpenQuestionDAO();
+                        openQuestionDAO.create(scan);
+                    }
+                    break;
+                case 2:
+                    ArrayList<String> topics = studentQuiz.getAllTopicsFromStudent(scan);
+                    int numberOfQuestions = studentQuiz.getTotalQuestionsCountFromStudent(scan);
+                    ArrayList<Question> questions = questionGenerator.getQuestions(quizType, topics, numberOfQuestions);
+                    QuizEvaluation quizEvaluation = new QuizEvaluation();
+                    int score = 0;
+                    int position = 0;
+                    File file = new File("D://Code2.txt");
+                    FileOutputStream outputStream = new FileOutputStream(file);
+                    PrintWriter pw = new PrintWriter(outputStream);
+                    pw.println("Quiz Type: " + quizType + "QUESTIONS");
+                    for (Question question : questions) {
+                        position++;
+                        pw.println(position + ". " + question.getTitle());
+                        boolean isCorrect = quizEvaluation.displayAndGetAnswer(pw, position, question);
+                        if (isCorrect) score++;
+                    }
+                    pw.flush();
+                    pw.close();
+                    System.out.println("Your score is: " + score);
+                    break;
+                case 3:
+                    if(quizType.equals("MCQ")){
+                        MCQDAO mcqDAO = new MCQDAO();
+                        mcqDAO.update(scan);
+                    }else if(quizType.equals("Open")){
+                        OpenQuestionDAO openQuestionDAO = new OpenQuestionDAO();
+                        openQuestionDAO.update(scan);
+                    }
+                    break;
+                case 4:
+                    if(quizType.equals("MCQ")){
+                        MCQDAO mcqDAO = new MCQDAO();
+                        mcqDAO.delete(scan);
+                    }else if(quizType.equals("Open")){
+                        OpenQuestionDAO openQuestionDAO = new OpenQuestionDAO();
+                        openQuestionDAO.delete(scan);
+                    }
+                    break;
+                case 5:
+                    System.out.println("Exiting");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
             }
-            pw.flush();
-            pw.close();
-            System.out.println("Your score is: " + score);
-
-
+//        String quizType = studentQuiz.getQuizTypeFromStudent(scan);
+//        if (quizType != null) {
+//            ArrayList<String> topics = studentQuiz.getAllTopicsFromStudent(scan);
+//            int numberOfQuestions = studentQuiz.getTotalQuestionsCountFromStudent(scan);
+//            ArrayList<Question> questions = questionGenerator.getQuestions(quizType,topics, numberOfQuestions);
+//            QuizEvaluation quizEvaluation = new QuizEvaluation();
+//            int score = 0;
+//            int position = 0;
+//            File file  = new File("D://Code2.txt");
+//            FileOutputStream outputStream = new FileOutputStream(file);
+//            PrintWriter pw = new PrintWriter(outputStream);
+//            pw.println("Quiz Type: " + quizType+ "QUESTIONS");
+//            for (Question question : questions) {
+//                position++;
+//                pw.println(position + ". " + question.getTitle());
+//                boolean isCorrect = quizEvaluation.displayAndGetAnswer(pw,position,question);
+//                if (isCorrect) score++;
+//            }
+//            pw.flush();
+//            pw.close();
+//            System.out.println("Your score is: " + score);
+//
+//
+//        }
         }
+
     }
-
-
 }
---
+++ src/fr/epita/quiz/services/data/dao/OpenQuestionDAO.java
@@ -0,0 +1,93 @@
+package fr.epita.quiz.services.data.dao;
+
+import fr.epita.quiz.datamodel.OpenQuestion;
+import fr.epita.quiz.datamodel.Question;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OpenQuestionDAO {
+    //create open question in database
+    public void create(Scanner scan) {
+        //insert questions into open question table
+        System.out.println("Enter question Id");
+        String id = scan.nextLine();
+        System.out.println("Enter question topic");
+        String topic = scan.nextLine();
+        System.out.println("Enter question title");
+        String title = scan.nextLine();
+        System.out.println("Enter question answer");
+        String answer = scan.nextLine();
+        try {
+            Connection con = connect();
+            java.sql.Statement stmt = con.createStatement();
+            String query = "INSERT INTO open_questions (sno,topic,question,answer) VALUES ("+id+",'"+topic+"','"+title+"','"+answer+"')";
+            stmt.executeUpdate(query);
+            System.out.println("Question Created successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //update open question in database
+    public void update(Scanner scan) {
+        //update questions in open question table
+        System.out.println("Enter question id");
+        String id = scan.nextLine();
+        System.out.println("Enter question topic");
+        String topic = scan.nextLine();
+        System.out.println("Enter question title");
+        String title = scan.nextLine();
+        System.out.println("Enter question answer");
+        String answer = scan.nextLine();
+        try {
+            Connection con = connect();
+            java.sql.Statement stmt = con.createStatement();
+            String query = "UPDATE open_questions SET topic = '"+topic+"', question = '"+title+"', answer = '"+answer+"' WHERE sno = '"+id+"'";
+            stmt.executeUpdate(query);
+            System.out.println("Question updated successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //delete open question from database
+    public void delete(Scanner scan) {
+        //delete questions from open question table
+        System.out.println("Enter question id");
+        String id = scan.nextLine();
+        try {
+            String query = "DELETE FROM open_questions WHERE sno=?";
+            Connection con = connect();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, id);
+            ps.executeUpdate();
+            System.out.println("Question deleted successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //get open question from database
+    public ArrayList<Question> getOpenQuestions(String topic, int qnPerTopic) throws SQLException {
+
+        String SQL = "SELECT * FROM open_questions where topic ='"+topic+"'order by random() limit "+qnPerTopic;
+        ArrayList<Question> questions = new ArrayList<Question>();    // ArrayList to store questions
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            while (rs.next()) {
+                OpenQuestion question = new OpenQuestion();
+                //question.setDescription(rs.getString("hint"));
+                question.setTitle(rs.getString("Question"));
+                question.setTopic(rs.getString("topic"));
+                question.setAnswer(rs.getString("answer"));
+                questions.add(question);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return questions;
+    }
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5433/Demo", "postgres","jean");
+    }
+}
